4483c0abaa716bf0c18d83c18c4c815bfd1675f1
mike011 <mike_ch_1@hotmail.com>
2024-01-12 15:00:14 -0500

    profie ui created

---
+++ ThreadsTutorial/Core/Profile/EditProfile/View/EditProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  EditProfileView.swift
+//  ThreadsTutorial
+//
+//  Created by Michael Charland on 2024-01-12.
+//
+
+import SwiftUI
+
+struct EditProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditProfileView()
+}
