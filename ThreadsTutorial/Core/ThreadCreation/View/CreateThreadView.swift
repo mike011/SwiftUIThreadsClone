1169f0882d1b7c5924533842e58e00462f270c2b
mike011 <mike_ch_1@hotmail.com>
2024-01-12 15:16:43 -0500

    CreateThreadView

--- ThreadsTutorial/Core/ThreadCreation/View/CreateThreadView.swift
+++ ThreadsTutorial/Core/ThreadCreation/View/CreateThreadView.swift
@@ -8,8 +8,62 @@
 import SwiftUI
 
 struct CreateThreadView: View {
+    @State private var caption = ""
+    @Environment(\.dismiss) var dismiss
+
     var body: some View {
-        Text("Create Thread")
+        NavigationStack {
+            VStack {
+                HStack(alignment: .top) {
+                    CircularProfileImageView()
+
+                    VStack(alignment: .leading, spacing: 4) {
+                        Text("Lisa")
+                            .fontWeight(.semibold)
+                        TextField("Start a thread...", text: $caption)
+                    }
+                    .font(.footnote)
+
+                    Spacer()
+
+                    if !caption.isEmpty {
+                        Button {
+                            caption = ""
+                        } label: {
+                            Image(systemName: "xmark")
+                                .resizable()
+                                .frame(width: 12, height: 12)
+                                .foregroundColor(.gray)
+                        }
+                    }
+                }
+
+                Spacer()
+            }
+            .padding()
+            .navigationTitle("New Thread")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button("Cancel") {
+                        dismiss()
+                    }
+                    .font(.subheadline)
+                    .foregroundColor(.black)
+                }
+
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("Post") {
+
+                    }
+                    .opacity(caption.isEmpty ? 0.5 : 1.0)
+                    .disabled(caption.isEmpty)
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                    .foregroundColor(.black)
+                }
+            }
+        }
     }
 }
 
